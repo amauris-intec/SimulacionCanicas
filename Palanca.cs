fa658905301c87e68242232cb8e3a6f04614aedb
Amauris Peña Díaz <amauris@outlook.com>
2022-11-25 22:56:08 -0400

    Add project files.

---
+++ Palanca.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimulacionCanicas
+{
+
+    internal interface IImpactable
+    {
+        public bool Impactar();
+    }
+
+    internal enum Posicion
+    {
+        izquierda,
+        derecha
+    }
+
+    internal enum MomentoCambio
+    {
+        antesDePasar,
+        luegoDePasar,
+        nunca
+    }
+
+    internal struct Coors
+    {
+        public Coors(int y, int x) => (this.Y, this.X) = (y, x);
+        public int Y { get; set; }
+        public int X { get; set; }
+    }
+
+    internal class Entrada
+    {
+        char input;
+        IImpactable destino;
+
+        public Entrada(char input, IImpactable destino) => (this.input, this.destino) = (input, destino);
+
+        public bool Lanzar() { return destino.Impactar(); }
+    }
+
+    internal class Salida : IImpactable
+    {
+        string id;
+        bool aceptacion;
+
+        public Salida(string id, bool aceptacion = false) => (this.id, this.aceptacion) = (id, aceptacion);
+
+        public bool Impactar() { return aceptacion; }
+    }
+
+    internal class Palanca : IImpactable
+    {
+        Coors coors;
+
+        string[] imagenIzquierda =  {@"   /",
+                                     @"  o ",
+                                     @" /  "};
+
+        string[] imagenDerecha = {@"\   ",
+                                  @" o  ",
+                                  @"  \ "};
+
+
+        string id;
+        MomentoCambio momentoCambio;
+        IImpactable? izquierda;
+        IImpactable? derecha;
+        Posicion posicion;
+        Posicion posicionInicial;
+
+        public string Id { get => id; }
+
+        public Palanca(string id, Coors coors, IImpactable? izquierda, IImpactable? derecha, Posicion posicionInicial, MomentoCambio momentoCambio = MomentoCambio.luegoDePasar)
+        {
+            this.id = id;
+            this.coors = coors;
+            this.izquierda = izquierda;
+            this.derecha = derecha;
+            this.posicion = this.posicionInicial = posicionInicial;
+            this.momentoCambio = momentoCambio;
+        }
+
+        private string[] ObtenerImagen() => posicion == Posicion.izquierda ? imagenIzquierda : imagenDerecha;
+
+        private IImpactable? ObtenerDestino() => posicion == Posicion.izquierda ? izquierda : derecha;
+
+        private void Cambiar() => posicion = posicion == Posicion.izquierda ? Posicion.derecha : Posicion.izquierda;
+
+        public void Reset() => posicion = posicionInicial;
+
+        public bool Impactar()
+        {
+            //Console.WriteLine($"{this.id} Impactado!");
+
+            if (momentoCambio == MomentoCambio.antesDePasar)
+            {
+                Cambiar();
+                //Console.WriteLine($"{this.id} cambiado hacia la {posicion.ToString()}");
+            }
+            IImpactable? destino = ObtenerDestino();
+            //Console.WriteLine($"Enviando canica hacia la {posicion.ToString()}");
+
+            if (momentoCambio == MomentoCambio.luegoDePasar) 
+            {
+                Cambiar();
+                //Console.WriteLine($"{this.id} cambiado hacia la {posicion.ToString()}");
+            }
+
+            if (destino != null)
+                return destino.Impactar();
+            else
+                return false;
+            
+        }
+
+        public string[] Dibujar(string[] fondo)
+        {
+            string[] imagen = ObtenerImagen();
+
+            int lineaFondo = coors.Y;
+            int lineaImagen = 0;
+            for (; lineaImagen < imagen.Length; lineaFondo++, lineaImagen++)
+                fondo[lineaFondo] = fondo[lineaFondo].Remove(coors.X, imagen[lineaImagen].Length).Insert(coors.X, imagen[lineaImagen]);
+            return fondo;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,125 @@
+﻿using SimulacionCanicas;
+    
+string[] fondo = @"
+                   A                B                   
+                |    |            |    |                
+                |    |            |    |                
+                |    |            |    |                
+                |    |            |    |                
+               /      \          /      \               
+              /        \        /        \              
+             /          \      /          \             
+            /     /\     \    /     /\     \            
+           /     /  \     \  /     /  \     \           
+          /     /    \     \/     /    \     \          
+         /     /      \          /      \     \         
+        /     /        \        /        \     \        
+        \     \        /        \        /     /        
+         \     \      /          \      /     /         
+          \     \    /     /\     \    /     /          
+           \     \  /     /  \     \  /     /           
+            \     \/     /    \     \/     /            
+             \          /      \          /             
+              \        /        \        /              
+               \      /          \      /               
+                |    |            |    |                
+                |    |            |    |                
+                |    |            |    |                
+                |    |            |    |                
+                |    |            |    |                
+                |    |            |    |                
+                   C                D                   ".Split('\n');
+
+Salida c = new Salida("C");
+Salida d = new Salida("D", aceptacion: true);
+
+Palanca x2 = new Palanca("x2", coors: new Coors(y: 13, x: 26), izquierda: c, derecha: d, posicionInicial: Posicion.izquierda);
+Palanca x1 = new Palanca("x1", coors: new Coors(y:  6, x: 17), izquierda: c, derecha: x2, posicionInicial: Posicion.izquierda);
+Palanca x3 = new Palanca("x3", coors: new Coors(y:  6, x: 35), izquierda: x2, derecha: d, posicionInicial: Posicion.izquierda);
+
+Entrada a = new Entrada('A', x1);
+Entrada b = new Entrada('B', x3);
+
+List<string> cadenasAceptadas = new List<string>();
+
+string cadena = "";
+bool aceptada = false;
+string msg = "";
+bool repetida = false;
+bool agregada = false;
+
+try
+{
+    do
+    {
+        fondo = x1.Dibujar(fondo);
+        fondo = x2.Dibujar(fondo);
+        fondo = x3.Dibujar(fondo);
+
+        Console.Clear();
+
+        if (aceptada)
+            Console.ForegroundColor = repetida ? ConsoleColor.Yellow : ConsoleColor.Green;
+        else
+            Console.ResetColor();
+
+        foreach (string linea in fondo)
+            Console.WriteLine(linea);
+
+        Console.WriteLine(msg);
+
+
+        ConsoleKeyInfo key = Console.ReadKey(true);
+
+        msg = aceptada && agregada ? "" : msg;
+        agregada = false;
+
+        if (key.KeyChar.ToString().ToLower() == "a")
+        {
+            cadena += "A";
+            msg += "A";
+            aceptada = a.Lanzar();
+            repetida = aceptada && cadenasAceptadas.Find(x => x.Equals(cadena)) != null;
+        }
+        else if (key.KeyChar.ToString().ToLower() == "b")
+        {
+            cadena += "B";
+            msg += "B";
+            aceptada = b.Lanzar();
+            repetida = aceptada && cadenasAceptadas.Find(x => x.Equals(cadena)) != null;
+        }
+        else if (key.Key == ConsoleKey.Escape)
+        {
+            Console.WriteLine($"Total de Cadenas encontradas: \n\t{string.Join("\n\t", cadenasAceptadas)}");
+            break;
+        }
+        else if (key.Key == ConsoleKey.Enter && aceptada)
+        {
+            agregada = true;
+            Console.WriteLine($"Total de Cadenas encontradas: \n\t{string.Join("\n\t", cadenasAceptadas)}");
+
+            if (cadenasAceptadas.Find(x => x.Equals(cadena)) != null)
+            { 
+                msg = $"Ya se había descubierto esta cadena: {cadena}";
+            }
+            else
+            {
+                msg = $"Nueva cadena descubierta!: {cadena}";
+                cadenasAceptadas.Add(cadena);
+            }
+
+            cadena = "";
+            x1.Reset();
+            x2.Reset();
+            x3.Reset();
+
+        }
+
+    }
+    while (true);
+
+}
+finally { Console.ResetColor(); };
+Console.WriteLine("Press any key to close...");
+Console.ReadKey(true);
+
